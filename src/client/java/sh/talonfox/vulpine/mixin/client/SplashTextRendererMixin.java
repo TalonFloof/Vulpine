06ba6e00c2901d481ec587a98b5dc61587a8f389
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-08-02 21:49:34 -0500

    Just a small final change

---
+++ src/client/java/sh/talonfox/vulpine/mixin/client/SplashTextRendererMixin.java
@@ -0,0 +1,26 @@
+package sh.talonfox.vulpine.mixin.client;
+
+import net.minecraft.client.gui.screen.SplashTextRenderer;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// Ya know why not?
+@Mixin(SplashTextRenderer.class)
+public class SplashTextRendererMixin {
+    @Mutable
+    @Shadow
+    @Final
+    private String text;
+
+    @Inject(method = "<init>(Ljava/lang/String;)V", at = @At("TAIL"))
+    public void foxesArePlayers(String text, CallbackInfo ci) {
+        if(this.text.equals("In case it isn't obvious, foxes aren't players.")) {
+            this.text = "In case it isn't obvious, foxes are players.";
+        }
+    }
+}
